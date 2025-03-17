04e4eae3c7a0c0941331fb78797ffc0cc14dfee2
daoduyquy321323 <daoduyquylop97@gmail.com>
2025-03-18 01:48:18 +0700

    home page

--- frontend/src/page/Home/index.js
+++ frontend/src/page/Home/index.js
@@ -1,7 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import LightImg from "../../assets/img/DenRGB.png";
+import EnvironmentImg from "../../assets/img/Environment.png";
+import FanRelayServoImg from "../../assets/img/FanRelayServo.png";
+import NotificationImg from "../../assets/img/Notification.png";
+import UserImg from "../../assets/img/TaiKhoan.png";
+import "./Home.css";
+
 const Home = () => {
+
+    const navigate = useNavigate();
+    const tabList = [
+        {
+            path: '/information-account',
+            icon: UserImg,
+            title: "Tài khoản",
+        },
+        {
+            path: '/light-rbg',
+            icon: LightImg,
+            title: "Đèn RGB",
+        },
+        {
+            path: '/environment',
+            icon: EnvironmentImg,
+            title: "Môi trường",
+        },
+        {
+            path: '/notification',
+            icon: NotificationImg,
+            title: "Thông báo",
+        },
+        {
+            path: '/fan-relay-servo',
+            icon: FanRelayServoImg,
+            title: "Quạt - Relay - Cửa",
+        },
+    ]
+
     return (
         <>
-            Home
+            <div className="container--3p container--home">
+                {
+                    tabList.map(item =>
+                        <div className="container--tab" onClick={() => navigate(item.path)}>
+                            <div className="logo">
+                                <img src={item.icon} alt={item.icon}/>
+                            </div>
+                            <h2>{item.title}</h2>
+                        </div>
+                    )
+                }
+            </div>
         </>
     )
 }
