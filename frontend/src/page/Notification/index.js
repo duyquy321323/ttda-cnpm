0733b4c101a33b17dab2a5bc5d3ae0f527beb128
minie111204 <167101474+minie111204@users.noreply.github.com>
2025-03-30 16:41:46 +0700

    fix: update Notification

--- frontend/src/page/Notification/index.js
+++ frontend/src/page/Notification/index.js
@@ -2,19 +2,37 @@ import React from "react";
 import fanservorelayIcon from "../../assets/img/notifications.png";
 import "./Notification.css";
 
+const notifications = [
+    { time: "2025/03/11 10:16:02AM", result: "Thất bại", device: "SERVO_DOOR", status: "Tắt", resultColor: "#FF2D55", statusColor: "#FF2D55" },
+    { time: "2025/03/11 10:15:52AM", result: "Thành công", device: "RELAY", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thành công", device: "SERVO_DOOR", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thất bại", device: "LED_RGB", status: "#0000FF", resultColor: "#FF2D55", statusColor: "#0000FF" },
+    { time: "2025/03/11 10:15:48AM", result: "Thất bại", device: "LED_RGB", status: "#FFFFFF", resultColor: "#FF2D55", statusColor: "#FFFFFF" },
+    { time: "2025/03/11 10:16:02AM", result: "Thất bại", device: "SERVO_DOOR", status: "Tắt", resultColor: "#FF2D55", statusColor: "#FF2D55" },
+    { time: "2025/03/11 10:15:52AM", result: "Thành công", device: "RELAY", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thành công", device: "SERVO_DOOR", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thất bại", device: "LED_RGB", status: "#0000FF", resultColor: "#FF2D55", statusColor: "#0000FF" },
+    { time: "2025/03/11 10:15:48AM", result: "Thất bại", device: "LED_RGB", status: "#FFFFFF", resultColor: "#FF2D55", statusColor: "#FFFFFF" },
+    { time: "2025/03/11 10:16:02AM", result: "Thất bại", device: "SERVO_DOOR", status: "Tắt", resultColor: "#FF2D55", statusColor: "#FF2D55" },
+    { time: "2025/03/11 10:15:52AM", result: "Thành công", device: "RELAY", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thành công", device: "SERVO_DOOR", status: "Bật", resultColor: "#34C759", statusColor: "#34C759" },
+    { time: "2025/03/11 10:15:49AM", result: "Thất bại", device: "LED_RGB", status: "#0000FF", resultColor: "#FF2D55", statusColor: "#0000FF" },
+    { time: "2025/03/11 10:15:48AM", result: "Thất bại", device: "LED_RGB", status: "#FFFFFF", resultColor: "#FF2D55", statusColor: "#FFFFFF" },
+];
+
 const Notification = () => {
     return (
         <div className="container--notification">
             {/* Intro */}
-            <div className = "intro">
-                <div className = "logo">
+            <div className="intro">
+                <div className="logo">
                     <img src={fanservorelayIcon} alt="Notification Icon" className="icon" />
                 </div>
-                <h1 className = "introTitle">
-                    THÔNG BÁO
-                </h1>
-                <p className = "introText">
-                    <span style={{ color: "var(--orangecolor)" }}>Lumi</span><span style={{ color: "var(--violetcolor)" }}>Home</span> hân hạnh được hỗ trợ <span style={{ color: "var(--violetcolor)" }}>"Thông báo"</span> để lưu trữ lịch sử sử dụng hệ thống nhà của bạn!
+                <h1 className="introTitle">THÔNG BÁO</h1>
+                <p className="introText">
+                    <span style={{ color: "var(--orangecolor)" }}>Lumi</span>
+                    <span style={{ color: "var(--violetcolor)" }}>Home</span> hân hạnh được hỗ trợ 
+                    <span style={{ color: "var(--violetcolor)" }}>"Thông báo"</span> để lưu trữ lịch sử sử dụng hệ thống nhà của bạn!
                 </p>
             </div>
 
@@ -32,126 +50,14 @@ const Notification = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:16:02AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#FF2D55" }}>Tắt</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:52AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>RELAY</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#0000FF" }}>#0000FF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:48AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#FFFFFFF" }}>#FFFFFF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:16:02AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#FF2D55" }}>Tắt</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:52AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>RELAY</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#0000FF" }}>#0000FF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:48AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#FFFFFFF" }}>#FFFFFF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:16:02AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#FF2D55" }}>Tắt</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:52AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>RELAY</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#0000FF" }}>#0000FF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:48AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#FFFFFFF" }}>#FFFFFF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:16:02AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#FF2D55" }}>Tắt</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:52AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>RELAY</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#34C759" }}>Thành công</td>
-                                    <td style={{ color: "#00AEEF" }}>SERVO_DOOR</td>
-                                    <td style={{ color: "#34C759" }}>Bật</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:49AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#0000FF" }}>#0000FF</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "#00AEEF" }}>2025/03/11 10:15:48AM</td>
-                                    <td style={{ color: "#FF2D55" }}>Thất bại</td>
-                                    <td style={{ color: "#00AEEF" }}>LED_RGB</td>
-                                    <td style={{ color: "#FFFFFFF" }}>#FFFFFF</td>
-                                </tr>
+                                {notifications.map((noti, index) => (
+                                    <tr key={index}>
+                                        <td style={{ color: "#00AEEF" }}>{noti.time}</td>
+                                        <td style={{ color: noti.resultColor }}>{noti.result}</td>
+                                        <td style={{ color: "#00AEEF" }}>{noti.device}</td>
+                                        <td style={{ color: noti.statusColor }}>{noti.status}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
